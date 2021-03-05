4a46cbe8295618ed112abf9248963e0fb59af0ae
Marin Aglic Cuvic <mrn.aglic@gmail.com>
2021-03-05 18:50:49 +0100

    Initial commit files

---
+++ TypequastAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TypequastAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
